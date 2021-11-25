4d115f3bab9e51228811891cb149d906a2f8a42b
BenzWachara <courager1996@gmail.com>
2021-11-26 00:06:18 +0700

    edit Bodyconent.js

--- src/components/Bodycontent.js
+++ src/components/Bodycontent.js
@@ -6,6 +6,7 @@ function Bodycontent(){
         <section className="body-content">
             <div className="content-card">
                 <h1>CARD KING</h1>
+                <h1>Kuyyy 55555</h1>
                 <h2>KING OF SPADES</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
